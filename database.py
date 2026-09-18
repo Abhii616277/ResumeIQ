2016c4204a1112ad76768502d8121d25273ff4b3
Abhii616277 <rj616277@gmail.com>
2026-09-18 11:00:28 +0530

    Fixed something

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 
 from config import MONGO_URI, MONGO_DB_NAME, MONGO_COLLECTION_NAME
 
-conn = MongoClient("mongodb+srv://rj616277_db_user:Rajkumar12@cluster0.qitfhku.mongodb.net/")
+conn = MongoClient(MONGO_URI)
 db = conn["Node"]
 collection = db["node"]
 fs = gridfs.GridFS(db)
